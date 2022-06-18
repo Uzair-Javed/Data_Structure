d2caec34a1417970f7591be60ea382c3411c8a1c
Uzair-Javed <82396774+Uzair-Javed@users.noreply.github.com>
2022-06-18 15:41:46 +0500

    Add files via upload

---
+++ Single_struct.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+struct Node {
+   int data;
+   struct Node *next;
+};
+struct Node* head = NULL;
+void insert(int new_data) {
+   struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+   new_node->data = new_data;
+   new_node->next = head;
+   head = new_node;
+}
+void display() {
+   struct Node* ptr;
+   ptr = head;
+   while (ptr != NULL) {
+      cout<< ptr->data <<" ";
+      ptr = ptr->next;
+   }
+}
+
+void populate()
+{
+   insert(500);
+   insert(400);
+   insert(300);
+   insert(200);
+   insert(100);
+}
+int main() {
+   
+   populate();
+   display();
+   return 0;
+}
